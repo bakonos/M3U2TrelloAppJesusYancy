98d537f2789835cdd909d545c435ec27f11e2334
Jesús Yancy <jyancy95@gmail.com>
2022-07-26 15:30:18 -0500

    creo request

---
+++ request.js
@@ -0,0 +1,10 @@
+const API_URL = "https://my-json-server.typicode.com/Stiwar9816/kanbanApp";
+
+axios
+  .get(`${API_URL}/tasks`)
+  .then((res) => showAllTasks(res.data))
+  .catch((err) => console.error(err));
+
+  const showAllTasks = (data) => {
+  data.map((task) => createTask(task));
+};
